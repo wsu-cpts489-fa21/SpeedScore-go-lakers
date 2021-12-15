3de5be5a45c7c63a76d8a92ee2aad1ab87ccf934
gzcccc1 <zicheng.gu@wsu.edu>
2021-12-14 17:16:25 -0800

    Test edit rounds. Including Edit strokes, time, and cancel, save button.

--- client/src/components/EditLiveRound.js
+++ client/src/components/EditLiveRound.js
@@ -56,19 +56,19 @@ class EditLiveRound extends React.Component {
               </div>
               <div id="live-round-form-center">
                     <u>Edit Strokes:</u>
-                    <button type="button" className={ this.state.stroke <= 199 ? "btn btn-dark" : "btn btn-opacity btn-dark disabled"} onClick={this.addStrokes}>+</button>
+                    <button type="button" id="Plus" className={ this.state.stroke <= 199 ? "btn btn-dark" : "btn btn-opacity btn-dark disabled"} onClick={this.addStrokes}>+</button>
                     <span>{this.state.stroke + " (Par)"}</span>
-                    <button type="button" className={this.state.stroke >= 2 ? "btn btn-dark" : "btn btn-opacity btn-dark disabled"} onClick={this.subtractStrokes}>-</button>
+                    <button type="button" id="Minus" className={this.state.stroke >= 2 ? "btn btn-dark" : "btn btn-opacity btn-dark disabled"} onClick={this.subtractStrokes}>-</button>
                     <u>Edit Time:</u>
                     <button type="button" className="btn btn-white" onClick={this.addTime}>+</button>
                     <span>{this.props.formatDate(this.state.time)}</span>
                     <button type="button" className={this.state.time >= 1000 ? "btn btn-white" : "btn btn-opacity btn-white disabled"} onClick={this.subtractTime}>-</button>
               </div>
               <div id="live-round-form-bottom">
-                <button type="button"  className="btn btn-success" onClick={this.saveChanges}>
+                <button type="button" id="Save"  className="btn btn-success" onClick={this.saveChanges}>
                      Save Changes
                 </button>
-                <button type="button" className="btn btn-danger" onClick={this.cancelChanges}>
+                <button type="button" id="Cancel" className="btn btn-danger" onClick={this.cancelChanges}>
                      Cancel Changes
                 </button>
               </div>
